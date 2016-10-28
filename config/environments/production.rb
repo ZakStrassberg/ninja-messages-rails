7dfffe7b457d00ec37de11d72618da3967d13aa3
Zak Strassberg <zakstrassberg@gmail.com>
2016-10-28 10:03:57 -0700

    env stuff for cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://ninja-messages.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://ninja-messages.herokuapp.com', 'http://ninja-messages.herokuapp.com' ]
+  config.web_socket_server_url = "wss://ninja-messages.herokuapp.com/cable"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
